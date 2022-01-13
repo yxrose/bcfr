6f4e7af1b4e2def8d1016c048888274b23d60383
Zhongmin Han <hanzm.vp@gmail.com>
2022-01-13 09:56:33 +0800

    Update retrench.py
    solved a bug when a chromosome only have one bin

--- retrench.py
+++ retrench.py
@@ -12,6 +12,8 @@ def retrench(key,cgt,rgc,nfd):
     regout=[]
     nbout=[]
     a=range(1,rgc.shape[0])
+    if len(a)==0 :
+        return (key, [rgc,cgc])
     ct=cgt.iloc[a[-1]*nfd:(a[-1]*nfd+nfd),:]
     current=ct.reset_index(level=0,drop=True)
     mix=ct.index
